a5434f90c9f7348cc4fba9e4dfeefc5004eb3844
aeen bolandian <bolandian85@gmail.com>
2025-02-27 13:32:54 +0330

    comments have been deleted

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -1,8 +1,5 @@
 public class Exercises {
 
-    /*
-        complete this function to check if the input number is prime or not
-     */
     public boolean isPrime(long n) {
         // todo
         if (n == 1)
@@ -15,12 +12,7 @@ public class Exercises {
         return false;
     }
 
-    /*
-        implement an algorithm to find out the index of input number in a fibonacci sequence starting from 0, 1
-        e.g. 0, 1, 1, 2, 3, 5, ...
-        indices start from 0, e.g. 3 is the index 4 of this sequence
-        if the input is not a fibonacci number with description above, return -1
-     */
+
     public long fibonacciIndex(long n) {
         // todo
         int first = 0;
@@ -43,24 +35,6 @@ public class Exercises {
         return -1;
     }
 
-    /*
-        you should create a triangle with "*" and return a two-dimensional array of characters based on that
-        the triangle's area is empty, which means some characters should be " "
-
-        example 1, input = 3:
-        *
-        **
-        ***
-
-        example 2, input = 5:
-        *
-        **
-        * *
-        *  *
-        *****
-
-        the output has to be a two-dimensional array of characters, so don't just print the triangle!
-     */
     public char[][] generateTriangle(int n) {
         // todo
         char[][] triangle = new char[n][];
